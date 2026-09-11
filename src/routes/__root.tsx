69321ed2ce2e94d4acd4e7ac48234a604a964968
assessoriaprimeoficial-tech <assessoriaprime.oficial@gmail.com>
2026-09-11 18:57:56 -0300

    Load required index.css to hide Lovable badge

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,6 +10,7 @@ import {
 import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
+import badgeCss from "../index.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 
 function NotFoundComponent() {
@@ -101,6 +102,10 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       {
         rel: "stylesheet",
         href: appCss,
+      },
+      {
+        rel: "stylesheet",
+        href: badgeCss,
       },
       {
         rel: "stylesheet",
